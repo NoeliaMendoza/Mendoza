a6b2a36b75ca14fbfe45b283e4791a4ae7f10631
Noelia Mendoza <nkmendoza@espe.edu.ec>
2026-01-13 00:30:49 -0500

    feat: menu de navegacion

---
+++ src/links.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Links</title>
+</head>
+<body>
+<nav>
+        <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="./post.php">Post</a></li>
+            <li><a href="./about.php">Sobre me</a></li>
+            <li><a href="./links.php">Proyecto</a></li>
+        </ul>
+    </nav>
+
+    <h1>Binvenidos a mi página de inicio</h1>
+    
+    
+</body>
+</html>
---
+++ src/resources/partials/navbar.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="./">Inicio</a></li>
+    <li><a href="./post.php">Post</a></li>
+    <li><a href="./about.php">Sobre me</a></li>
+    <li><a href="./links.php">Proyecto</a></li>
+</ul>
